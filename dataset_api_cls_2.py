7897405bff5c7652e6af5da65be0c3088f1999b5
waddah27 <waddahkh.ali94@gmail.com>
2024-08-02 00:41:01 +0300

    added normailzation transforms to images and masks

--- dataset_api_cls_2.py
+++ dataset_api_cls_2.py
@@ -9,10 +9,15 @@ class ObjectDataset(Dataset):
         self.root = root
         self.image_dir = os.path.join(root, 'images')
         self.mask_dir = os.path.join(root, 'masks')
-        self.transform = transforms.Compose([
+        self.transform_image = transforms.Compose([
             transforms.Resize((128, 128)),
             transforms.ToTensor(),
-            # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        self.transform_mask = transforms.Compose([
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485], std=[0.229])
         ])
         self.image_files = [f for f in os.listdir(self.image_dir) if f.endswith('.jpg')]
         self.mask_files = [f for f in os.listdir(self.mask_dir) if f.endswith('.png')]
@@ -33,8 +38,8 @@ class ObjectDataset(Dataset):
         mask_arr = np.array(mask)
         label = 1 if np.any(mask_arr) else 0
 
-        image = self.transform(image)
-        mask = self.transform(mask)
+        image = self.transform_image(image)
+        mask = self.transform_mask(mask)
 
         return image, label, mask
 
